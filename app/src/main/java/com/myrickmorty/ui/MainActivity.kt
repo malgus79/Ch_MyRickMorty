e2221a68a3a21e4d343375246005cd690cd66557
Gustavo Maldonado <gamcba7982@gmail.com>
2023-01-10 21:18:34 -0300

    implemented bottom navigation

--- app/src/main/java/com/myrickmorty/ui/MainActivity.kt
+++ app/src/main/java/com/myrickmorty/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.snackbar.Snackbar
 import com.myrickmorty.R
 import com.myrickmorty.core.ConnectionLiveData
@@ -25,13 +26,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupNavigation()
         checkNetworkConnection()
 
+    }
+
+    private fun setupNavigation() {
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
     }
 
     private fun checkNetworkConnection() {
@@ -55,9 +67,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.popBackStack()
-
-    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.popBackStack()
+//
+//    }
 }
